677d02f9db7193037255e64c39cb5bf068014e77
Vikash Sharma <rjvikash001@gmail.com>
2018-06-04 01:58:44 +0530

    Project for D-zone

---
+++ app/src/main/java/com/example/gaps/continentsandoceans/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gaps.continentsandoceans;
+
+import android.support.v7.app.AppCompatActivity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
