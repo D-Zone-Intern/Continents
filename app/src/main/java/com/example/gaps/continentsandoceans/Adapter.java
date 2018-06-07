3888ef37b35eb09c1e9313af60cd36194e02614a
Vikash Sharma <rjvikash001@gmail.com>
2018-06-07 23:06:14 +0530

    Layout

---
+++ app/src/main/java/com/example/gaps/continentsandoceans/Adapter.java
@@ -0,0 +1,50 @@
+package com.example.gaps.continentsandoceans;
+
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+public class Adapter extends PagerAdapter{
+
+
+        private ArrayList<Integer> images;
+        private LayoutInflater inflater;
+
+    Adapter(Context context, ArrayList<Integer> images) {
+        this.images=images;
+        inflater = LayoutInflater.from(context);
+    }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+
+        @Override
+        public int getCount() {
+        return images.size();
+    }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup view, int position) {
+        View myImageLayout = inflater.inflate(R.layout.slider, view, false);
+        ImageView myImage = (ImageView) myImageLayout
+                .findViewById(R.id.image);
+            myImage.setImageResource(images.get(position));
+        view.addView(myImageLayout, 0);
+        return myImageLayout;
+    }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+    }
--- app/src/main/java/com/example/gaps/continentsandoceans/MainActivity.java
+++ app/src/main/java/com/example/gaps/continentsandoceans/MainActivity.java
@@ -1,15 +1,57 @@
 package com.example.gaps.continentsandoceans;
 
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.location.Location;
 import android.location.LocationListener;
+import com.google.android.gms.maps.LocationSource;
+import com.google.android.gms.common.ConnectionResult;
+import me.relex.circleindicator.CircleIndicator;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final Integer[] pics = {R.drawable.s1, R.drawable.s2};
+    private  ViewPager mPager;
+    private static int currentPage = 0;
+    private ArrayList<Integer> picsArray = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        Collections.addAll(picsArray, pics);
+
+        mPager = findViewById(R.id.pager);
+        mPager.setAdapter(new Adapter(this, picsArray));
+        CircleIndicator indicator = findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+
+        // Auto start of viewpager
+        new Handler();
+        new Runnable() {
+            public void run() {
+                if (currentPage == pics.length) {
+                    currentPage = 0;
+                }
+                mPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
     }
 }
