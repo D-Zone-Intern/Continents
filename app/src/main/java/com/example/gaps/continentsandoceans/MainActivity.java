cfcc550fdf3b605cccc0cbaed301a8866dd51627
Vikash Sharma <rjvikash001@gmail.com>
2018-07-06 12:21:29 +0530

    new change card view

--- app/src/main/java/com/example/gaps/continentsandoceans/MainActivity.java
+++ app/src/main/java/com/example/gaps/continentsandoceans/MainActivity.java
@@ -165,32 +165,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-            sharingIntent.setType("text/plain");
-            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Continents and Oceans");
-            startActivity(Intent.createChooser(sharingIntent, "Share via"));
-
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
 
 
--- app/src/main/java/com/example/gaps/continentsandoceans/OceansActivity.java
+++ app/src/main/java/com/example/gaps/continentsandoceans/OceansActivity.java
@@ -3,6 +3,7 @@ package com.example.gaps.continentsandoceans;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,7 +12,7 @@ import android.widget.ImageButton;
 public class OceansActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    ImageButton indianocean,southernocean,pacificocean,arcticocean,atlanticocean;
+    CardView indianocean,southernocean,pacificocean,arcticocean,atlanticocean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +23,11 @@ public class OceansActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     private void init() {
-        indianocean = findViewById(R.id.indianocean);
-        southernocean = findViewById(R.id.southernocean);
-        pacificocean = findViewById(R.id.pacificocean);
-        arcticocean = findViewById(R.id.arcticocean);
-        atlanticocean = findViewById(R.id.atlantisocean);
+        indianocean = findViewById(R.id.indian_ocean);
+        southernocean = findViewById(R.id.southern_ocean);
+        pacificocean = findViewById(R.id.pacific_ocean);
+        arcticocean = findViewById(R.id.arctic_ocean);
+        atlanticocean = findViewById(R.id.atlantis_ocean);
 
         indianocean.setOnClickListener(this);
         southernocean.setOnClickListener(this);
@@ -39,27 +40,27 @@ public class OceansActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.indianocean:
+            case R.id.indian_ocean:
                 Intent intent = new Intent(this, Indian_ocean.class);
                 intent.putExtra("dataa","Indian Ocean");
                 startActivity(intent);
                 break;
-            case R.id.southernocean :
+            case R.id.southern_ocean :
                 Intent i = new Intent(this, southern_ocean.class);
                 i.putExtra("dataa","Southern Ocean");
                 startActivity(i);
                 break;
-            case R.id.pacificocean :
+            case R.id.pacific_ocean :
                 Intent in = new Intent(this, pacific_ocean.class);
                 in.putExtra("dataa","Pacific Ocean");
                 startActivity(in);
                 break;
-            case R.id.arcticocean :
+            case R.id.arctic_ocean :
                 Intent inte = new Intent(this, arctic_ocean.class);
                 inte.putExtra("dataa","Arctic Ocean");
                 startActivity(inte);
                 break;
-            case R.id.atlantisocean :
+            case R.id.atlantis_ocean :
                 Intent inten = new Intent(this, atlantic_ocean.class);
                 inten.putExtra("dataa","Atlantic Ocean");
                 startActivity(inten);
@@ -70,32 +71,6 @@ public class OceansActivity extends AppCompatActivity implements View.OnClickLis
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-            sharingIntent.setType("text/plain");
-            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Continents and Oceans");
-            startActivity(Intent.createChooser(sharingIntent, "Share via"));
-
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
 
 
